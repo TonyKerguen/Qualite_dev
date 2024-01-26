48d753259f9614dc023d672b6d56aa5b92ba82be
Kerguen Tony <o22205496@info25-08.iut45.univ-orleans.fr>
2024-01-26 11:01:47 +0100

    ajout de toute les fonction en rapport avec rectangle et ses tests

--- tp1/geom.py
+++ tp1/geom.py
@@ -9,3 +9,37 @@ def getx(p):
 
 def gety(p):
 	return p["y"]
+
+
+def rectangle(x,y,w,h):
+	res=dict()
+	res["x"]=x
+	res["y"]=y
+	res["w"]=w
+	res["h"]=h
+	return res
+
+def getxrectangle(rectangle):
+	return rectangle["x"]
+
+def getyrectangle(rectangle):
+        return rectangle["y"]
+
+def getwrectangle(rectangle):
+        return rectangle["w"]
+
+def gethrectangle(rectangle):
+        return rectangle["h"]
+
+def get_center(rectangle):
+	return point(getxrectangle(rectangle)+getwrectangle(rectangle)/2,getyrectangle(rectangle)+gethrectangle(rectangle)/2)
+
+def get_surface(rectangle):
+	return gethrectangle(rectangle)*getwrectangle(rectangle)
+
+def set_center(rectangle, p):
+	rectangle["x"]=2*p[0]-rectangle["w"]
+	rectangle["y"]=2*p[1]-rectangle["h"]
+	return rectangle
+
+print(set_center(rectangle(0,0,2,2),(1.5,1.5)))
--- tp1/test_point.py
+++ tp1/test_point.py
@@ -4,3 +4,23 @@ def test_creation():
 	p = point(22,7)
 	assert 22 == getx(p)
 	assert 7 == gety(p)
+
+rectangle_test = rectangle(0,0,2,2)
+
+def test_getxrectangle():
+	assert getxrectangle(rectangle_test) == 0
+
+def test_getyrectangle():
+	assert getyrectangle(rectangle_test) == 0
+
+def test_getwrectangle():
+	assert getwrectangle(rectangle_test) == 2
+
+def test_gethrectangle():
+	assert gethrectangle(rectangle_test) == 2
+
+def test_get_center():
+	assert get_center(rectangle_test) == {'x': 1.0, 'y': 1.0}
+
+def test_set_center():
+	assert set_center(rectangle_test, (1.5,1.5)) == {'x': 1.0, 'y': 1.0, 'w': 2, 'h': 2}
